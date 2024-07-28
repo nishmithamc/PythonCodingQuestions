feeb8fe62463522b24e9b95d7d97e5803af2c687
shreyasmc <shreyasmc2000@gmail.com>
2024-07-28 12:01:02 +0530

    first commit

---
+++ Even_Or_Odd.py
@@ -0,0 +1,9 @@
+i=int(input("Please Enter a Number :"))
+
+if i % 2 == 0:
+    print(f"{i} is  a Even Number") #fstring
+
+else:
+    print(i,"is a Odd Number")
+
+#Python code to check whether the given number is Odd or Even
